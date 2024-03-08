c19adf7af2c9a0000e78487bf99c625c4b1b579d
Ilona Gasiewska <i.gasiewska@godeltech.com>
2024-03-08 17:19:20 +0100

    PROXY-7: Add logic to your app

--- index.js
+++ index.js
@@ -3,35 +3,74 @@ process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 const fetch = require('node-fetch');
 
 const express = require('express');
-const send = require('send');
+const { relative } = require('path');
 const app = express();
 const port = 4000;
 
 
-const key = require("./variables/api.js").key;
-const url = require("./variables/api.js").url;
+// const key = require("./variables/api.js").key;
+// const url = require("./variables/api.js").url;
 
-const isKeyValid = key != "" ? true : false
+// const isKeyValid = key != "" ? true : false
 
-isKeyValid === false && process.exit(1);
+// isKeyValid === false && process.exit(1);
 
-fetch(url)
-    .then(res => res.json())
+
+   
+app.get('/meteors/:startDay/:endDay/:key', (req, res) => {
+    const userParameters = {
+        startDay: req.params.startDay,
+        endDay: req.params.endDay,
+        key: req.params.key
+    }
+
+    const days = getDays(userParameters.startDay, userParameters.endDay);
+    const url  = `https://api.nasa.gov/neo/rest/v1/feed?start_date=${userParameters.startDay}&end_date=${userParameters.endDay}&api_key=${userParameters.key}`;
+
+    fetch(url)
+    .then(respons => respons.json())
     .then(data => {
-        asteroidsNumber = data.element_count;
-        sendData(asteroidsNumber);
+      res.send(getData(data, days));
     })
     .catch(err => {
-        return err.message;
+        console.log(err.message);
     })
-
-const sendData = (asteroidsNumber) => {
-   
-    app.get('/meteors', (req, res) => {
-        res.send(`Nasa have seen ${asteroidsNumber} asteroids.`);
-    })
+})
         
-    app.listen(port, () => {
-         console.log(`Server is running on port ${port}`);
-    });
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
+
+
+const getDays = (startDay, endDay) => {
+    const days = [];
+    let currentDate = new Date(startDay);
+
+    while (currentDate <= new Date(endDay)){
+        days.push(new Date(currentDate).toISOString().substring(0, 10));
+        currentDate.setDate(currentDate.getDate() +1)
+    }
+    return days;
 }
+
+const getData = (data, days) => {
+
+    let asteroidsData = [];
+    days.forEach(day => {
+        data.near_earth_objects[day].map(asteroid=> {
+            asteroidsData.push( {
+            id: asteroid.id,
+            name : asteroid.name,
+            diameter: ( asteroid.estimated_diameter.kilometers.estimated_diameter_max - asteroid.estimated_diameter.kilometers.estimated_diameter_min),
+            isPotentiallyHazardousAsteroid: asteroid.is_potentially_hazardous_asteroid,
+            close_approach_date_full: asteroid.close_approach_data[0].close_approach_date_full,
+            relative_velocity: asteroid.close_approach_data[0].relative_velocity.kilometers_per_second
+            })
+        })
+    });
+
+    return asteroidsData
+}
+
+
+// http://localhost:4000/meteors/2015-02-21/2015-02-23/262o2PcvlfW9FNXzZSqNaW9a0uQ7yUGExY7Sc0
--- variables/api.js
+++ variables/api.js
@@ -1,5 +1,4 @@
+// const key = "262o2PcvlfW9FNXzZSqNaW9a0uQ7yUGExY7Sc0yP";
+// const url = `https://api.nasa.gov/neo/rest/v1/feed?start_date=2015-09-07&end_date=2015-09-08&api_key=${key}`;
 
-const key = "262o2PcvlfW9FNXzZSqNaW9a0uQ7yUGExY7Sc0yP";
-const url = `https://api.nasa.gov/neo/rest/v1/feed?start_date=2015-09-07&end_date=2015-09-08&api_key=${key}`;
-
-module.exports = { key, url }
+// module.exports = { key, url }
